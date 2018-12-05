29eb9d130db198738b96f895bf32151244c018e4
durkeepk <pdurkee@utexas.edu>
2018-12-05 09:49:21 -0600

    remove raw-data folder

--- data-raw/add_data.R
+++
@@ -1,8 +0,0 @@
-#### Adding Datasets to a Package
-
-# 1. Load in the dataset from raw_data
-
-sloper_exdat <- read.csv("data-raw/sloper_exdat.csv")
-
-# 2. Create the RData object & Add to the data folder
-usethis::use_data(sloper_exdat, overwrite = T)
